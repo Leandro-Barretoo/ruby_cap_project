37ac0ae6d41e4c4734d144e5d0c927080dfa97b3
Leandro-Barretoo <officialdevmi@gmail.com>
2021-01-26 08:41:09 +0000

    Fix bug and some rubocop offenses

--- lib/commands/reply_question.rb
+++ lib/commands/reply_question.rb
@@ -1,9 +1,9 @@
 class Reply < SlackRubyBot::Bot
 	command 'reply' do |channel, data, _match|
-		channel_name = data.text.split(' ', 3)[1]
-		reply_text = data.text.split(' ', 3)[-1]
+		channel_name = data.text.split(' ', 4)[2]
+		reply_text = data.text.split(' ', 4)[-1]
 
-		uri = URI.parse("https://slack.com/api/conversations.list")
+		uri = URI.parse('https://slack.com/api/conversations.list')
 		request = Net::HTTP::Get.new(uri)
 		request.content_type = 'application/x-www-form-urlencoded'
 		request['Authorization'] = "Bearer #{ENV['SLACK_API_TOKEN']}"
@@ -16,14 +16,12 @@ class Reply < SlackRubyBot::Bot
 		ans = JSON.parse(response.body)
 
 		channel_id = 0
-		ans["channels"].each do |i|
-			if i["name"] == channel_name
-				channel_id = i["id"]
-			end
+		ans['channels'].each do |i|
+			channel_id = i['id'] if i['name'] == channel_name
 		end
 
 		client = Slack::Web::Client.new
-		uri = URI("https://slack.com/api/conversations.history")
+		uri = URI('https://slack.com/api/conversations.history')
 		params = {
 			token: ENV['SLACK_API_TOKEN'],
 			channel: channel_id,
@@ -33,7 +31,7 @@ class Reply < SlackRubyBot::Bot
 		uri.query = URI.encode_www_form(params)
 		res = Net::HTTP.get_response(uri)
 		res_one = JSON.parse(res.body)
-		quest_ts = res_one["messages"][0]["ts"]
+		quest_ts = res_one['messages'][0]['ts']
 		client.chat_postMessage(channel: "#{channel_id}", text: "#{reply_text}", as_user: true, thread_ts: "#{quest_ts}")
 	end
 end
